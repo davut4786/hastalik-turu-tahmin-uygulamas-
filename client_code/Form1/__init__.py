1190b13a94492f6b544d78820a8565d0972ae326
d.seyhan19786@gmail.com <d.seyhan19786@gmail.com>
2024-10-30 18:53:27 +0000

    Edited ServerModule1, Form1 and custom package dependencies

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,114 +1,35 @@
-from ._anvil_designer import Form1Template
-from anvil import *
+import pickle
+import urllib.request
 import anvil.server
+import pandas as pd
 
-class Form1(Form1Template):
-    def __init__(self, **properties):
-        self.init_components(**properties)
+# Google Drive'dan modeli indirme
+def load_model_from_drive():
+    url = "https://drive.google.com/uc?id=1V26NN9pxPHbpIrfpQN271T36PPaIYqkU"  # Güncellenmiş URL
+    model_path = "/tmp/hastalikturu_model.pkl"  # Geçici dosya yolu
+    urllib.request.urlretrieve(url, model_path)  # Model dosyasını indir
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)  # Modeli yükle
+    return model
 
-        # Dropdown'lar için seçenekler
-        self.tur_dropdown.items = [
-            ("Kedi", 0),
-            ("Köpek", 1)
-        ]
-        
-        self.sistem_dropdown.items = [
-            ("Bilinmiyor", 0),
-            ("Boşaltım", 1),
-            ("Deri", 2),
-            ("Dolaşım", 3),
-            ("Mix (2'den fazla sistem)", 4),
-            ("Sindirim", 5),
-            ("Sinir", 6),
-            ("Solunum", 7)
-        ]
+# Modeli yükleme
+model = load_model_from_drive()  # Bir kez indirip bellekte tut
 
-        self.abdominal_agri_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
+@anvil.server.callable
+def model_tahmin(features):
+    # Özellikleri DataFrame'e çevirme
+    columns = ['tur', 'sistem', 'cBasebC', 'cBaseEcfc', 'HCO3Pc', 'p50c', 
+               'cHCO3Pst', 'cNa', 'FHHb', 'sO2', 'GRAN', 'LYM', 'MON_A', 
+               'HCT', 'MCH', 'MCHC', 'abdominal_agri', 'genel_durum', 
+               'idar_problemi', 'inkordinasyon', 'ishal', 'istahsizlik', 
+               'kanama', 'kusma', 'oksuruk']
+    
+    input_data = pd.DataFrame([features], columns=columns)  # Özellikleri DataFrame'e çevir
 
-        self.genel_durum_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
+    # Kategorik değişkenleri sayısal hale getirme (one-hot encoding)
+    input_data = pd.get_dummies(input_data, drop_first=True)
 
-        self.idar_problemi_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
+    # Tahmin yapma
+    prediction = model.predict(input_data)
 
-        self.inkordinasyon_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
-
-        self.ishal_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
-
-        self.istahsizlik_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
-
-        self.kanama_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
-
-        self.kusma_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
-
-        self.oksuruk_dropdown.items = [
-            ("Yok", 0),
-            ("Var", 1)
-        ]
-
-    def tahmin_et_button_click(self, **event_args):
-        """Bu metod butona tıklandığında çağrılır"""
-        
-        # Kullanıcının girdiği değerleri alın
-        tur = self.tur_dropdown.selected_value
-        sistem = self.sistem_dropdown.selected_value
-        
-        # Float değerlerini alın
-        cBasebC = float(self.cBasebC_textbox.text)
-        cBaseEcfc = float(self.cBaseEcfc_textbox.text)
-        HCO3Pc = float(self.HCO3Pc_textbox.text)
-        p50c = float(self.p50c_textbox.text)
-        cHCO3Pst = float(self.cHCO3Pst_textbox.text)
-        cNa = float(self.cNa_textbox.text)
-        FHHb = float(self.FHHb_textbox.text)
-        sO2 = float(self.sO2_textbox.text)
-        GRAN = float(self.GRAN_textbox.text)
-        LYM = float(self.LYM_textbox.text)
-        MON_A = float(self.MON_A_textbox.text)
-        HCT = float(self.HCT_textbox.text)
-        MCH = float(self.MCH_textbox.text)
-        MCHC = float(self.MCHC_textbox.text)
-        
-        # Dropdown değerlerini alın
-        abdominal_agri = self.abdominal_agri_dropdown.selected_value
-        genel_durum = self.genel_durum_dropdown.selected_value
-        idar_problemi = self.idar_problemi_dropdown.selected_value
-        inkordinasyon = self.inkordinasyon_dropdown.selected_value
-        ishal = self.ishal_dropdown.selected_value
-        istahsizlik = self.istahsizlik_dropdown.selected_value
-        kanama = self.kanama_dropdown.selected_value
-        kusma = self.kusma_dropdown.selected_value
-        oksuruk = self.oksuruk_dropdown.selected_value
-        
-        # Model tahmini yapma
-        sonuc = anvil.server.call(
-            'model_tahmin', 
-            [tur, sistem, cBasebC, cBaseEcfc, HCO3Pc, p50c, cHCO3Pst, cNa, FHHb, sO2, GRAN, LYM, MON_A, HCT, MCH, MCHC, abdominal_agri, genel_durum, idar_problemi, inkordinasyon, ishal, istahsizlik, kanama, kusma, oksuruk]
-        )
-
-        # Sonucu Textbox'ta göster
-        self.sonuc_textbox.text = f"Tahmin Sonucu(%85): {sonuc}"
-
-# `tahmin_et_button` butonunun click olayına yukarıdaki metod bağlanmalıdır.
+    return prediction[0]  # Tekil bir sonuç döndür
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,26 +1,114 @@
+from ._anvil_designer import Form1Template
+from anvil import *
 import anvil.server
-import pickle
-import urllib.request
 
-# GitHub’dan modeli indirme işlevi
-def load_model_from_github():
-    url = "https://raw.githubusercontent.com/davut4786/HastalikTuru/main/hastalikturu_model.pkl"  # GitHub URL'niz
-    model_path = "/tmp/hastalikturu_model.pkl"  # Geçici dosya yolu
-    urllib.request.urlretrieve(url, model_path)  # Modeli indir
-    with open(model_path, 'rb') as file:
-        model = pickle.load(file)  # Modeli yükle
-    return model
+class Form1(Form1Template):
+    def __init__(self, **properties):
+        self.init_components(**properties)
 
-# Modeli yükleme
-model = load_model_from_github()  # Modeli bellekte tut
+        # Dropdown'lar için seçenekler
+        self.tur_dropdown.items = [
+            ("Kedi", 0),
+            ("Köpek", 1)
+        ]
+        
+        self.sistem_dropdown.items = [
+            ("Bilinmiyor", 0),
+            ("Boşaltım", 1),
+            ("Deri", 2),
+            ("Dolaşım", 3),
+            ("Mix", 4),
+            ("Sindirim", 5),
+            ("Sinir", 6),
+            ("Solunum", 7)
+        ]
 
-@anvil.server.callable
-def model_tahmin(features):
-    # Kategorik değişkenleri sayısal hale getirme (one-hot encoding) burada kaldırılıyor
-    # Özellikleri doğrudan kullanıyoruz
-    # Özellikler, modelin beklediği formatta olmalı
-    # features bir liste olmalı
-    # Tahmin yapma
-    prediction = model.predict([features])
+        self.abdominal_agri_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
 
-    return prediction[0]  # Tekil bir sonuç döndür
+        self.genel_durum_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
+
+        self.idar_problemi_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
+
+        self.inkordinasyon_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
+
+        self.ishal_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
+
+        self.istahsizlik_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
+
+        self.kanama_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
+
+        self.kusma_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
+
+        self.oksuruk_dropdown.items = [
+            ("Yok", 0),
+            ("Var", 1)
+        ]
+
+    def tahmin_et_button_click(self, **event_args):
+        """Bu metod butona tıklandığında çağrılır"""
+        
+        # Kullanıcının girdiği değerleri alın
+        tur = self.tur_dropdown.selected_value
+        sistem = self.sistem_dropdown.selected_value
+        
+        # Float değerlerini alın
+        cBasebC = float(self.cBasebC_textbox.text)
+        cBaseEcfc = float(self.cBaseEcfc_textbox.text)
+        HCO3Pc = float(self.HCO3Pc_textbox.text)
+        p50c = float(self.p50c_textbox.text)
+        cHCO3Pst = float(self.cHCO3Pst_textbox.text)
+        cNa = float(self.cNa_textbox.text)
+        FHHb = float(self.FHHb_textbox.text)
+        sO2 = float(self.sO2_textbox.text)
+        GRAN = float(self.GRAN_textbox.text)
+        LYM = float(self.LYM_textbox.text)
+        MON_A = float(self.MON_A_textbox.text)
+        HCT = float(self.HCT_textbox.text)
+        MCH = float(self.MCH_textbox.text)
+        MCHC = float(self.MCHC_textbox.text)
+        
+        # Dropdown değerlerini alın
+        abdominal_agri = self.abdominal_agri_dropdown.selected_value
+        genel_durum = self.genel_durum_dropdown.selected_value
+        idar_problemi = self.idar_problemi_dropdown.selected_value
+        inkordinasyon = self.inkordinasyon_dropdown.selected_value
+        ishal = self.ishal_dropdown.selected_value
+        istahsizlik = self.istahsizlik_dropdown.selected_value
+        kanama = self.kanama_dropdown.selected_value
+        kusma = self.kusma_dropdown.selected_value
+        oksuruk = self.oksuruk_dropdown.selected_value
+        
+        # Model tahmini yapma
+        sonuc = anvil.server.call(
+            'model_tahmin', 
+            [tur, sistem, cBasebC, cBaseEcfc, HCO3Pc, p50c, cHCO3Pst, cNa, FHHb, sO2, GRAN, LYM, MON_A, HCT, MCH, MCHC, abdominal_agri, genel_durum, idar_problemi, inkordinasyon, ishal, istahsizlik, kanama, kusma, oksuruk]
+        )
+
+        # Sonucu Textbox'ta göster
+        self.sonuc_textbox.text = f"Tahmin Sonucu: {sonuc}"
+
+# `tahmin_et_button` butonunun click olayına yukarıdaki metod bağlanmalıdır.
